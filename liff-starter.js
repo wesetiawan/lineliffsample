e9e9569f490829a30b36949f3e89053854d34a04
wawan eka setiyawan <wawaneka567@gmail.com>
2020-02-15 19:16:53 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -3,7 +3,7 @@ window.onload = function() {
     const defaultLiffId = "1653856641";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
-    let myLiffId = "";
+    let myLiffId = "1653856641";
 
     // if node is used, fetch the environment variable and pass it to the LIFF method
     // otherwise, pass defaultLiffId
